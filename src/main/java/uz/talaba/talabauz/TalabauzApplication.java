15038d5b2ebe5abae288a86e5677e63518a304bf
Muminniyoz Shonazarov <muminniyoz2912@gmail.com>
2021-06-18 21:07:46 +0500

    Birinchi

---
+++ src/main/java/uz/talaba/talabauz/TalabauzApplication.java
@@ -0,0 +1,13 @@
+package uz.talaba.talabauz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalabauzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalabauzApplication.class, args);
+    }
+
+}
